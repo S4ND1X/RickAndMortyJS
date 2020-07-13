e705e3523622e67243d67bc89eb6a223d6c5bc65
S4NDIX <sandix390@gmail.com>
2020-07-13 00:18:41 -0500

    Added api function at load

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ import API from "./api.js";
 const api = new API();
 
 //* Div container of the character template
-const characterContainer = document.querySelector(".character-container");
+const characterContainer = document.getElementById("character-container");
 
 //* Wait for page to load
 document.addEventListener("DOMContentLoaded", initApp);
@@ -20,11 +20,18 @@ class Character {
   setCharacterTemplate() {
     return `
             <article class="character">
-                <div class="character-grid">
-                    <h1>${this.character.name}</h1>
-                    <img src="${this.character.image}" alt="">
+            <div class="character-grid">
+                <div class="character-name">
+                    <h2 id="name">${this.character.name}</h2>
                 </div>
-            </article>`;
+                <img class="character-img" src="${this.character.image}" alt="">
+                <div class="character-labels">
+                    <h3 id="origin" class="character-label">Origin: ${this.character.origin.name}</h3>
+                    <h3 id="origin" class="character-label">Specie: ${this.character.species}</h3>
+                    <h3 id="status" class="character-label">Status:${this.character.status}</h3>
+                </div>
+            </div>
+        </article>`;
   }
 
   //* Method to display character on screen
@@ -36,7 +43,7 @@ class Character {
 //* Get the first character when page is loaded
 function initApp() {
   api.getCharacter(1).then((character) => {
-    // new Character(character);
-    console.log(character);
+    console.log(characterContainer);
+    new Character(character);
   });
 }
